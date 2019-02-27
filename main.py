78a7a2581fb2cd4982c8fcaa32f17700dce927fd
Cory Prelerson <cory.prelerson@darkwolfsolutions.com>
2019-02-27 17:41:30 -0500

    Load UI class created

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-import sys
+import sys, os
  
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
+from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtCore import QFile, Slot
-
+from uiloader import loadUi
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         loadUi('mainwindow.ui', self)
 
-def load_playbook_clicked():
-    print("load_playbook_clicked")
- 
+    @Slot(bool)
+    def on_clickMe_clicked(self, is_checked):
+        if is_checked:
+            message = self.trUtf8(b'I am checked now.')
+        else:
+            message = self.trUtf8(b'I am unchecked now.')
+        QMessageBox.information(self, self.trUtf8(b'You clicked me'), message)
+
+    @Slot()
+    def load_playbook_clicked(self):
+        print("load_playbook_clicked")
+
+    @Slot()
+    def load_inventory_clicked(self):
+        print("load_inventory_clicked")
+
 if __name__ == "__main__":
+
+
+#     app = QApplication(sys.argv)
+#     file = QFile("mainwindow.ui")
+#     file.open(QFile.ReadOnly)
+#     loader = QUiLoader()
+#     window = loader.load(file)
+#     window.btn_load_inventory.clicked.connect(load_playbook_clicked)
+#     window.show()
+#     sys.exit(app.exec_())
+
     app = QApplication(sys.argv)
-    file = QFile("mainwindow.ui")
-    file.open(QFile.ReadOnly)
-    loader = QUiLoader()
-    window = loader.load(file)
-    window.btn_load_inventory.clicked.connect(load_playbook_clicked)
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
