64cd91dcd657e770628d6999779458900e5f13b8
Cory Prelerson <cory.prelerson@darkwolfsolutions.com>
2019-02-27 17:25:05 -0500

    WIP commit

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import sys
  
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QApplication
-from PySide2.QtCore import QFile
- 
+from PySide2.QtCore import QFile, Slot
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        loadUi('mainwindow.ui', self)
+
+def load_playbook_clicked():
+    print("load_playbook_clicked")
  
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -11,5 +20,6 @@ if __name__ == "__main__":
     file.open(QFile.ReadOnly)
     loader = QUiLoader()
     window = loader.load(file)
+    window.btn_load_inventory.clicked.connect(load_playbook_clicked)
     window.show()
     sys.exit(app.exec_())
