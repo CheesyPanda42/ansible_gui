6ef9ddc9ade57712b557c1eb59c5ce7e3e9859bb
Cory Prelerson <cory.prelerson@darkwolfsolutions.com>
2019-02-28 14:01:01 -0500

    Change name of text boxes, add file dialog

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys, os
+import sys, os, pathlib
  
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PySide2.QtCore import QFile, Slot
 from uiloader import loadUi
 
@@ -12,21 +12,20 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self, parent)
         loadUi('mainwindow.ui', self)
 
-    @Slot(bool)
-    def on_clickMe_clicked(self, is_checked):
-        if is_checked:
-            message = self.trUtf8(b'I am checked now.')
-        else:
-            message = self.trUtf8(b'I am unchecked now.')
-        QMessageBox.information(self, self.trUtf8(b'You clicked me'), message)
-
     @Slot()
     def load_playbook_clicked(self):
         print("load_playbook_clicked")
+        self.get_file()
 
     @Slot()
     def load_inventory_clicked(self):
         print("load_inventory_clicked")
+        self.get_file()
+
+    def get_file(self):
+        homedir = pathlib.Path.home()
+        filename = QFileDialog.getOpenFileName(self, "Select Ansible file", str(homedir), "Ansible files (*.yml)")
+        print(filename)
 
 if __name__ == "__main__":
 
